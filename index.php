3fdc68d6e8fafd57314f55aa8773ea8e8ec60c83
Kevin Barkutwa <barkutwa@gmail.com>
2015-10-14 15:38:19 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Photo Reader</title>
+</head>
+<body>
+	<div style="padding:30px;">
+		<p>Upload unedited Photo to read EXIF Details</p>
+		<form action="readImage.php" method="post" enctype="multipart/form-data">
+		<input type="file" name="file"/>
+		<input type="submit" value="Upload"/>
+	</div>
+
+</body>
+</html>
